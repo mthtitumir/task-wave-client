bdedcd43c5ba85ed7369f4c327117be23165e4eb
mthtitumir <titumirgccc@gmail.com>
2023-10-07 21:50:05 +0600

    development branch set

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../layouts/Main";
 import FeaturePage from "../pages/FeaturePage/FeaturePage";
 import PricingPage from "../pages/PricingPage/PricingPage";
+import Test from "../components/test";
 
 export const router = createBrowserRouter([
     {
@@ -18,5 +19,9 @@ export const router = createBrowserRouter([
             }
         ]
     },
+    {
+        path: '/test',
+        element: <Test></Test>
+    }
 
 ])
